3a6243a3d3221265c6ca4b4d4162b26487baff38
Arukala Sai Kamal <104195391+Kamal0078k@users.noreply.github.com>
2026-07-08 16:23:21 +0530

    Updated Number

--- src/Quotation/Head.jsx
+++ src/Quotation/Head.jsx
@@ -5,7 +5,7 @@ const Head = () => {
     <div className="w-[100%] flex flex-col justify-center items-center gap-4">
       <div className="text-[10px] ml-[70%]">
         <div>Prop: Suresh</div>
-        <div>Cell: 996376533, 9515992534</div>
+        <div>Cell: 9963765333, 9515992534</div>
         <div>Mail: bhagyalaxmissfabrications@gmail.com</div>
       </div>
       <div className="text-red-500  text-4xl border-[0.1rem] rounded-md   w-[100%] text-center py-2 border-black">
